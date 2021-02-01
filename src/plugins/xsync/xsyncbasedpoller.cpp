0629ef40afe59b8fac63f97eb46fb521d21a380b
Nicolas Fella <nicolas.fella@gmx.de>
2021-02-01 14:02:26 +0100

    Handle Qt6 change in nativeEventFilter signature
    The last parameter's type changed from long* to qintptr*

--- src/plugins/xsync/xsyncbasedpoller.cpp
+++ src/plugins/xsync/xsyncbasedpoller.cpp
@@ -24,7 +24,11 @@ public:
     {
         delete q;
     }
+#if QT_VERSION >= QT_VERSION_CHECK(6,0,0)
+    bool nativeEventFilter(const QByteArray &eventType, void *message, qintptr *result) override
+#else
     bool nativeEventFilter(const QByteArray &eventType, void *message, long *result) override
+#endif
     {
         Q_UNUSED(result);
         if (isActive && eventType == "xcb_generic_event_t") {
